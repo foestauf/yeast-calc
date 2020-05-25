b07bf739399322848c612aa53bffcccd9e70b645
foestauf <46908845+foestauf@users.noreply.github.com>
2020-05-25 20:33:59 +0000

    Deploying to gh-pages from  @ 2c666f98e0b55efa3c4ebd5e70b86b5bcff5b240 🚀

---
+++ precache-manifest.ead2ba7cedbce86ec40a928c717623ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6636bd0d5afb29a31844e5cca19556d",
+    "url": "/yeast-calc/index.html"
+  },
+  {
+    "revision": "7fe517f9af459edb3ef8",
+    "url": "/yeast-calc/static/css/main.ab70fcf0.chunk.css"
+  },
+  {
+    "revision": "0796ef98707c2cc2d3b5",
+    "url": "/yeast-calc/static/js/2.106d5a90.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/yeast-calc/static/js/2.106d5a90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fe517f9af459edb3ef8",
+    "url": "/yeast-calc/static/js/main.747cd2a9.chunk.js"
+  },
+  {
+    "revision": "e097a3bb5c8923a31365",
+    "url": "/yeast-calc/static/js/runtime-main.f46a3fea.js"
+  }
+]);
