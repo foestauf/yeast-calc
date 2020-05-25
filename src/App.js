2c666f98e0b55efa3c4ebd5e70b86b5bcff5b240
foestauf <46908845+foestauf@users.noreply.github.com>
2020-05-25 16:31:32 -0400

    Initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,212 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useReducer, useState} from "react";
+import {Container} from "react-bootstrap";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialState = {
+  fermVol: 10,
+  dillution: 200,
+  cellConc: 1.0,
+  densSlurry: 1.1,
+  numSquares: 25
 }
 
-export default App;
+const yeastReducer = (state, action) => {
+  switch (action.type) {
+    case 'FERMVOL':
+      return {...state, fermVol: action.payload};
+    case 'GRAVITY':
+      return {...state, gravity: action.payload};
+    case 'CELLCONC':
+      return {...state, cellConc: action.payload};
+    case 'DENSSLURRY':
+      return {...state, densSlurry: action.payload};
+    case 'NUMSQUARES':
+      return {...state, numSquares: action.payload};
+    case 'DILLUTION':
+      return {...state, dillution: action.payload};
+    case 'LIVECELLS':
+      return {...state, liveCells: action.payload}
+    case 'DEADCELLS':
+      return {...state, deadCells: action.payload}
+    default:
+      throw new Error('WTF OVER')
+  }
+}
+
+const YeastCalculator = () => {
+  const [state, dispatch] = useReducer(yeastReducer, initialState);
+  const [output, setOutput] = useState({});
+  function calc() {
+    let cellsCounted = (state.liveCells + state.deadCells)
+    let viability = (state.liveCells / cellsCounted * 100).toFixed(2)
+    let squares = 25 / state.numSquares
+    let fermVolML = (state.fermVol * 31) * 3785.41;
+    let cellsPerML = cellsCounted * squares * state.dillution * Math.pow(10,4)
+    let cellConcPerMLPerP = (state.cellConc * Math.pow(10,6))
+    let totalCellsPitch = state.fermVol * 117180 * state.gravity * Math.pow(10, 5)
+    let volNeededML = totalCellsPitch / cellsPerML
+    let volNeededLiter = volNeededML / 1000
+    let volNeededGal = volNeededLiter * 0.264172
+    let massNeededGram = volNeededML / state.densSlurry
+    let massNeededKG = massNeededGram / 1000
+    let massNeededLB = massNeededKG * 2.20462
+    setOutput({...output,
+      viability: viability,
+      cellsCounted: cellsCounted,
+      cellsML: cellsPerML.toExponential(2),
+      fermGal: state.fermVol * 31,
+      fermML: fermVolML.toExponential(2),
+      cellConcPerMLPerP: cellConcPerMLPerP.toExponential(2),
+      cellConcML: (cellConcPerMLPerP * state.gravity).toExponential(2),
+      totalCellsPitch: totalCellsPitch.toExponential(2),
+      volNeededML: volNeededML.toExponential((2)),
+      volNeededLiter: volNeededLiter.toFixed(2),
+      volNeededGal: volNeededGal.toFixed(2),
+      massNeededGram: massNeededGram.toExponential(2),
+      massNeededKG: massNeededKG.toFixed(2),
+      massNeededLB: massNeededLB.toFixed(2)
+    })
+  }
+  useEffect(calc,[state]);
+  return (
+      <Container style={{marginTop: "30px"}} id="yeast-calc">
+        <Row>
+          <Col className="input">Yellow Cells are inputs</Col>
+          <Col className="output">Blue Cells are calculated</Col>
+        </Row>
+        <Row>
+          <Col className="leftAnchor-col">
+            <Row className="topRow">Fermentation Volume (in BBL)</Row>
+            <Row className="input btmCol">
+              <Form.Control placeholder={state.fermVol}
+                            onChange={e => dispatch({
+                              type: 'FERMVOL',
+                              payload: Number(e.target.value)
+                            })} size="sm" type="text"/></Row>
+          </Col>
+          <Col>This is size of equipment</Col>
+          <InfoCell title="Cell Count (Cells/ML)" value={output.cellsML}/>
+          <InfoCell title="Fermentation Volume (gal)" value={output.fermGal}/>
+          <InfoCell title="Fermentation Volume (mL)" value={output.fermML}/>
+          <InfoCell title="Desired Cell Concentration (Cells/mL-P)" value={output.cellConcPerMLPerP}/>
+          <InfoCell title="Desired Cell Concentration (Cells/mL)" value={output.cellConcML}/>
+          <InfoCell title="Total # Cells for Pitch" value={output.totalCellsPitch}/>
+        </Row>
+        <Row>
+          <Col className="leftAnchor-col">
+            <Row className="topRow">Desired Gravity</Row>
+            <Row className="input">
+              <Form.Control onChange={e => dispatch({
+                type: 'GRAVITY',
+                payload: Number(e.target.value)
+              })} size="sm"
+                            type="text"/></Row>
+          </Col>
+          <Col>This is the expected gravity of the beer being pitched into</Col>
+          <InfoCell title="Volume of Slurry Needed (mL)" value={output.volNeededML}/>
+          <InfoCell title="Volume of Slurry needed (L)" value={output.volNeededLiter}/>
+          <InfoCell title="Volume of Slurry Needed (Gal)" value={output.volNeededGal}/>
+          <InfoCell title="Mass of slurry Needed(g)" value={output.massNeededGram}/>
+          <InfoCell title="Mass of slurry needed (KG)" value={output.massNeededKG}/>
+          <InfoCell title="Mass of slurry needed (lb)" value={output.massNeededLB}/>
+        </Row>
+        <Row>
+          <Col className="leftAnchor-col">
+            <Row className="topRow">Desired Cell concentration (Cells/mL x 1E6)</Row>
+            <Row className="input">
+              <Form.Control placeholder={state.cellConc}
+                            onChange={e => dispatch({
+                                  type: 'CELLCONC',
+                                  payload: Number(e.target.value)
+                                }
+                            )} size="sm"
+                            type="text"/></Row>
+          </Col>
+          <Col>This is the desired concentration of cells/mL</Col>
+          <Col className="leftAnchor-col" md={9}>
+            <Row className="topRow"></Row>
+            <Row style={{display: "inline"}}>Hemocytometer cell count</Row>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="leftAnchor-col">
+            <Row className="topRow">Density of Slurry (g/mL)</Row>
+            <Row className="input">
+              <Form.Control placeholder={state.densSlurry}
+                            onChange={e => dispatch({
+                              type: 'DENSSLURRY',
+                              payload: Number(e.target.value)
+                            })} size="sm"
+                            type="text"/></Row>
+          </Col>
+          <Col>
+            <Row>This is the density of the slury being pitched. This can be measured of if unknown go with
+              1.1g/mL</Row>
+          </Col>
+          <InfoCell title="Total Cells Counted" value={output.cellsCounted}/>
+          <Col>
+            <Row className="topRow"># of Squares Counted</Row>
+            <Row className="input">
+              <Form.Control placeholder={state.numSquares} onChange={e => dispatch({
+                type: 'NUMSQUARES',
+                payload: Number(e.target.value)
+              })} size="sm"
+                            type="text"/></Row>
+          </Col>
+          <Col>
+            <Row className="topRow">Dilution</Row>
+            <Row className="input">
+              <Form.Control placeholder={state.dillution}
+                            onChange={e => dispatch({
+                              type: 'DILLUTION',
+                              payload: Number(e.target.value)
+                            })} size="sm"
+                            type="text"/></Row>
+          </Col>
+          <Col>
+            <Row className="topRow">Live Cells Counted</Row>
+            <Row className="input">
+              <Form.Control placeholder={state.liveCells}
+                            onChange={e => dispatch({
+                              type: 'LIVECELLS',
+                              payload: Number(e.target.value)
+                            })} size="sm"
+                            type="text"/></Row>
+          </Col>
+          <Col>
+            <Row className="topRow">Dead Cells Counted</Row>
+            <Row className="input">
+              <Form.Control placeholder={state.deadCells}
+                            onChange={e => dispatch({
+                              type: 'DEADCELLS',
+                              payload: Number(e.target.value)
+                            })} size="sm"
+                            type="text"/></Row>
+          </Col>
+          <InfoCell title="% Viability (Do not pitch under 90%)" value={output.viability}/>
+        </Row>
+      </Container>
+  )
+}
+
+const InfoCell = (props) => {
+  function isNaN(n) {
+    if (n >= 0) {
+      return n
+    } else {
+      return '-'
+    }
+  }
+  return (
+      <Col>
+        <Row className="topRow">{props.title}</Row>
+        <Row className="output">
+          <Form.Control size="sm" type="text" readOnly placeholder="-"
+                        value={isNaN(props.value)}/></Row>
+      </Col>
+  )
+}
+
+export default YeastCalculator;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './index.scss';
 import * as serviceWorker from './serviceWorker';
+import YeastCalculator from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <YeastCalculator />
   </React.StrictMode>,
   document.getElementById('root')
 );
